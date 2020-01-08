f3e99f130a595592fee9a6bf5fd82b068a8c7c11
sriram10 <msriram1010@gmail.com>
2020-01-09 00:36:50 +0530

    update github pages config - add CNAME

--- site-config.js
+++ site-config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Pentafox Technologies`,
-  siteTitleShort: `Pentafox`,
+  siteTitle: `Pentafox Technologies | Design the Digital World`,
+  siteTitleShort: `Pentafox | Design the Digital World`,
   siteDescription: `Pentafox Technologies is a Data age IT product solutions startup focused on transforming all kinds of business to the next level with the the power of Cloud embedded with intelligence from wide range of AI/ML capabilities.`,
   siteUrl: `https://pentafox.in`,
   themeColor: `#000`,
@@ -11,7 +11,8 @@ module.exports = {
   author: `@sriram1010`,
   logo: path.resolve(__dirname, 'src/images/logo.svg'),
   social: {
-    twitter: `pentafox-twitter`,
-    fbAppId: `pentafox-fb`,
+    twitter: `pentafox-tech`,
+    linkedin: `pentafox-tech`,
+    fbAppId: `pentafox-tech`,
   },
 };
